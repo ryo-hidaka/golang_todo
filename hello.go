b27423348f79f2ef9659712c0b76ea8bf6a64e5f
ryo-hidaka <percussion665@gmail.com>
2020-07-12 12:34:36 +0900

    “pushコマンドの確認”

---
+++ hello.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"golang_todo/models"
+
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/volatiletech/sqlboiler/boil"
+	. "github.com/volatiletech/sqlboiler/queries/qm"
+)
+
+func GetDataTodo(ctx context.Context, c *gin.Context) {
+	var b models.Todo
+	if err := c.Bind(&b); err != nil {
+		fmt.Errorf("%#v", err)
+	}
+	b.Status = 0
+	err := b.InsertG(ctx, boil.Infer())
+
+	todos, err := models.Todos().AllG(ctx)
+	if err != nil {
+		fmt.Errorf("Get todo error: %v", err)
+	}
+	c.HTML(http.StatusOK, "index.html", map[string]interface{}{
+		"todo": todos,
+	})
+}
+
+func GetDoneTodo(ctx context.Context, c *gin.Context) {
+	var b models.Todo
+	if err := c.Bind(&b); err != nil {
+		fmt.Errorf("%#v", err)
+	}
+
+	if b.Status == 0 {
+		println("haiteru")
+		b.Status = 1
+	} else {
+		b.Status = 0
+	}
+
+	af, err := b.UpdateG(ctx, boil.Whitelist("status", "updated_at"))
+	println("Get todo error: %v", af)
+	if err != nil {
+		fmt.Errorf("Get todo error: %v", err)
+	}
+
+	todos, err := models.Todos().AllG(ctx)
+	if err != nil {
+		fmt.Errorf("Get todo error: %v", err)
+	}
+	c.HTML(http.StatusOK, "index.html", map[string]interface{}{
+		"todo": todos,
+	})
+
+}
+
+func main() {
+	ctx := context.Background()
+	db, err := sql.Open("mysql", "root:529440@tcp(localhost:3306)/todo?parseTime=true")
+	if err != nil {
+		log.Fatalf("Cannot connect database: %v", err)
+	}
+	boil.SetDB(db)
+
+	r := gin.Default()
+	r.LoadHTMLFiles("./index/index.html")
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "world",
+		})
+	})
+	r.GET("/todo", func(c *gin.Context) {
+
+		todos, err := models.Todos(OrderBy("updated_at desc")).AllG(ctx)
+		if err != nil {
+			fmt.Errorf("Get todo error: %v", err)
+		}
+
+		c.HTML(http.StatusOK, "index.html", map[string]interface{}{
+			"todo": todos,
+		})
+	})
+	r.GET("/todoContent", func(c *gin.Context) {
+		GetDataTodo(ctx, c)
+	})
+	r.GET("/done", func(c *gin.Context) {
+		GetDoneTodo(ctx, c)
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
